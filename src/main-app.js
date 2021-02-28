119a15309eebec502a8c08036c4c5d8de3999dc3
Ander Suarez <me@andersuarez.com>
2021-02-28 23:44:26 +0100

    Add ejercicio5 solution

--- src/main-app.js
+++ src/main-app.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import MoviesScreen from 'screens/movies';
+import MovieDetailScreen from 'screens/movie-detail';
+import { routes } from 'utils/constants';
 
 const App = () => {
-  return <MoviesScreen />;
+  return (
+    <Switch>
+      <Route path={routes.home} exact>
+        <MoviesScreen />
+      </Route>
+      <Route path={routes.movieDetail}>
+        <MovieDetailScreen />
+      </Route>
+      <Route path={routes.all}>
+        <Redirect to={routes.home} />
+      </Route>
+    </Switch>
+  );
 };
 
 export default App;
---
+++ src/screens/movie-detail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import GeneralLayout from 'components/general-layout';
+
+const MovieDetail = () => {
+  return (
+    <GeneralLayout>
+      <p>Soy el detalle de una peli</p>
+    </GeneralLayout>
+  );
+};
+
+export default MovieDetail;
