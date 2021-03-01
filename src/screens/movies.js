e0ebc77fbfa079a2e077ee93b61dac363ba83495
Ander Suarez <me@andersuarez.com>
2021-03-01 13:21:10 +0100

    Solve ej 3
    Co-authored-by: superseti <superseti@users.noreply.github.com>
    Co-authored-by: Alejandro González Gutierrez <aggutierrez@plainconcepts.com>
    Co-authored-by: David Gómez Ruiz <dgruiz@plainconcepts.com>
    Co-authored-by: Francho Joven <fjoven@plainconcepts.com>
    Co-authored-by: Iker Obregon Reigosa <IObregon@users.noreply.github.com>
    Co-authored-by: André Pérez Fandiño <adperez@plainconcepts.com>
    Co-authored-by: Ander Conal Fuertes <aconal@plainconcepts.com>
    Co-authored-by: Denis Sola <dsola@plainconcepts.com>
    Co-authored-by: Naroa Agirre Pujana <nagirre@plainconcepts.com>
    Co-authored-by: Erlantz Oniga Ouro <eoniga@plainconcepts.com>

--- src/screens/movies.js
+++ src/screens/movies.js
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import MovieListItem from 'components/movie-list-item';
 import GeneralLayout from 'components/general-layout';
 import Input from 'components/input';
 import apiClient from 'utils/api-client';
 
-// TODO: Reescribeme usando useState y useEffect
-class Movies extends React.Component {
+const Movies = () => {
+  const [movies, setMovies] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+  const [query, setQuery] = useState('');
+
+  useEffect(() => {
+    apiClient('api/movie').then(
+      (data) => {
+        setMovies(data.movies);
+        setLoaded(true);
+      },
+      (error) => {
+        setLoaded(true);
+      },
+    );
+  }, []);
+
+  const filteredMovies = movies.filter((movie) =>
+    movie.title.startsWith(query),
+  );
+
+  const updateQuery = (e) => {
+    setQuery(e.currentTarget.value);
+  };
+
+  return (
+    <GeneralLayout>
+      <div className="mb-4">
+        <Input value={query} onChange={updateQuery} />
+      </div>
+      {!loaded && <p>Loading movies</p>}
+      {loaded && filteredMovies.length
+        ? filteredMovies.map((movie) => (
+            <MovieListItem title={movie.title} key={movie._id} />
+          ))
+        : null}
+      {loaded && !filteredMovies.length ? (
+        <p>There are no movies with the required results</p>
+      ) : null}
+    </GeneralLayout>
+  );
+};
+class MoviesOld extends React.Component {
   constructor(props) {
     super(props);
     this.state = { query: '', movies: [], loading: false, loaded: false };
