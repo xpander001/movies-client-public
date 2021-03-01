4b97d26af0d5000b77380e71f7b815e7df59bc5e
Ander Suarez <me@andersuarez.com>
2021-03-01 01:04:19 +0100

    Add ejercicio 10 base

---
+++ ejercicios/Ejercicio 10.md
@@ -0,0 +1,7 @@
+# Ejercicio 10
+
+Ya tenemos el login. Sin embargo, la única forma de hacer logout es via devtools (lol), y no podemos navegar entre login y signup.
+
+Vamos a modificar el header para mostrar un botón que haga logout si estás logueado, y que permita navegar entre login y signup.
+
+De paso, aprenderemos a usar el hook useMatch de React Router para indicar en qué página estamos.
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import classnames from 'classnames';
+import { Link as RouterLink, useRouteMatch } from 'react-router-dom';
+import { routes } from 'utils/constants';
+import Button from 'components/button';
 
-const Header = () => {
+const HeaderLink = ({ path, children }) => {
+  const match = useRouteMatch({
+    path,
+  });
+  const linkClass = classnames({
+    'p-2': true,
+    border: match,
+  });
+  return (
+    <span className={linkClass}>
+      <RouterLink to={path}>{children}</RouterLink>
+    </span>
+  );
+};
+
+const Header = ({ user, logout }) => {
   return (
     <header className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-body border-bottom shadow-sm">
       <p className="h5 my-0 me-md-auto flex-fill">My movies</p>
+      {user ? (
+        <>
+          <div className="mr-2">Hello, {user.name}</div>
+          <Button primary outline onClick={logout}>
+            Logout
+          </Button>
+        </>
+      ) : (
+        <nav className="my-2 my-md-0 me-md-3">
+          <HeaderLink path={routes.login}>Login</HeaderLink>
+          <HeaderLink path={routes.signup}>Sign Up</HeaderLink>
+        </nav>
+      )}
     </header>
   );
 };
