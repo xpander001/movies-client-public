704efa8876812f22a61d6a9268e7f3c05668929f
Ander Suarez <me@andersuarez.com>
2021-03-01 12:36:14 +0100

    WIP
    Co-authored-by: superseti <superseti@users.noreply.github.com>
    Co-authored-by: Alejandro González Gutierrez <aggutierrez@plainconcepts.com>
    Co-authored-by: David Gómez Ruiz <dgruiz@plainconcepts.com>
    Co-authored-by: Francho Joven <fjoven@plainconcepts.com>
    Co-authored-by: Iker Obregon Reigosa <IObregon@users.noreply.github.com>
    Co-authored-by: André Pérez Fandiño <adperez@plainconcepts.com>
    Co-authored-by: Ander Conal Fuertes <aconal@plainconcepts.com>
    Co-authored-by: Denis Sola <dsola@plainconcepts.com>
    Co-authored-by: Naroa Agirre Pujana <nagirre@plainconcepts.com>
    Co-authored-by: Erlantz Oniga Ouro <eoniga@plainconcepts.com>

--- src/screens/movies.js
+++ src/screens/movies.js
@@ -3,95 +3,61 @@ import MovieListItem from 'components/movie-list-item';
 import GeneralLayout from 'components/general-layout';
 import Input from 'components/input';
 import Button from 'components/button';
-
-const movies = {
-  movies: [
-    {
-      _id: '60381f292d88eb50bc5a3618',
-      title: '10,000 B.C.',
-      description: 'This is a description',
-    },
-    {
-      _id: '60381f292d88eb50bc5a3616',
-      title: '102 Dalmatians',
-      description: 'This is a description',
-    },
-    {
-      _id: '60381f292d88eb50bc5a3619',
-      title: '10th & Wolf',
-      description: 'This is a description',
-    },
-    {
-      _id: '60381f292d88eb50bc5a361a',
-      title: '11:14',
-      description: 'This is a description',
-    },
-    {
-      _id: '60381f282d88eb50bc5a3207',
-      title: '12 Angry Men',
-      description: 'This is a description',
-    },
-    {
-      _id: '60381f292d88eb50bc5a361c',
-      title: '12 Rounds',
-      description: 'This is a description',
-    },
-    {
-      _id: '60381f292d88eb50bc5a361e',
-      title: '13 Going On 30',
-      description: 'This is a description',
-    },
-    {
-      _id: '60381f292d88eb50bc5a3620',
-      title: '1408',
-      description: 'This is a description',
-    },
-    {
-      _id: '60381f292d88eb50bc5a3621',
-      title: '15 Minutes',
-      description: 'This is a description',
-    },
-    {
-      _id: '60381f292d88eb50bc5a3623',
-      title: '16 Blocks',
-      description: 'This is a description',
-    },
-  ],
-  totalPages: 321,
-  currentPage: 1,
-};
+import apiClient from 'utils/api-client';
 
 class Movies extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { query: '' };
+    this.state = {
+      query: '',
+      movies: [],
+      loaded: false,
+    };
 
     this.updateQuery = this.updateQuery.bind(this);
   }
 
+  componentDidMount() {
+    apiClient('api/movie').then((data) =>
+      this.setState({
+        ...this.state,
+        movies: data.movies,
+        loaded: true,
+      }),
+    );
+  }
+
   updateQuery(e) {
-    this.setState({ query: e.currentTarget.value });
+    this.setState({ ...this.state, query: e.currentTarget.value });
   }
 
   render() {
-    const filteredMovies = movies.movies.filter((movie) =>
-      movie.title.startsWith(this.state.query),
+    const { movies, loaded, query } = this.state;
+    const filteredMovies = movies.filter((movie) =>
+      movie.title.startsWith(query),
     );
+
     return (
       <GeneralLayout>
         <div className="mb-4">
-          <Input value={this.state.query} onChange={this.updateQuery} />
+          <Input label="Search" value={query} onChange={this.updateQuery} />
         </div>
-        {filteredMovies.length ? (
-          filteredMovies.map((movie) => (
-            <MovieListItem title={movie.title} key={movie._id} />
-          ))
-        ) : (
-          <p>There are no movies with the required results</p>
-        )}
+        {!loaded && <p>Loading</p>}
+
+        {filteredMovies.map((movie) => (
+          <MovieListItem title={movie.title} key={movie._id} />
+        ))}
+        <NoResults loaded={loaded} moviesLength={filteredMovies.length} />
       </GeneralLayout>
     );
   }
 }
+
+const NoResults = ({ loaded, moviesLength }) => {
+  if (loaded && !moviesLength) {
+    return <p>No hay pelis colega</p>;
+  }
+  return null;
+};
 
 export default Movies;
